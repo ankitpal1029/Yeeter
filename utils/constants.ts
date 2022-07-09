bbec16acafe0b8522a85e154bc324249436ea867
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-07-09 09:58:25 +0530

    deployment ready

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,11 @@
 import socialMediaLocalDeployments from "../utils/deployments/localhost/SocialMedia.json";
+import socialMediaTestNetDeployments from "../utils/deployments/rinkeby/SocialMedia.json";
 
 export const socialMediaContract =
-  process.env.NODE_ENV === "development" ? socialMediaLocalDeployments.abi : "";
+  process.env.NODE_ENV === "development"
+    ? socialMediaLocalDeployments.abi
+    : socialMediaTestNetDeployments.abi;
 export const socialMediaAddress =
   process.env.NODE_ENV === "development"
     ? socialMediaLocalDeployments.address
-    : "";
+    : socialMediaTestNetDeployments.address;
